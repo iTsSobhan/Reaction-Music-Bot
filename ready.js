b9d8c7b381f3405d1b5476cd0776a3b221ac4006
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-10-14 22:30:55 +0330

    Delete ready.js

--- ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (client) => {
-  console.log(`${client.guilds.cache.size} Servers`);
-  await client.user.setActivity("-play", {
-    type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
-  });
-};
-
