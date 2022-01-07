bb322586469b0431d20ead85818a1d67340744f8
Sobhan.SRZA <90289153+Sobhan-SRZA@users.noreply.github.com>
2022-01-07 18:47:18 +0330

    Update serverlist.js

--- commands/serverlist.js
+++ commands/serverlist.js
@@ -7,8 +7,10 @@ module.exports = {
   name: "serverlist",
   aliases: ["sl"],
   description: i18n.__('list of all servers bot join'),
-  execute(message,client) {
-        const Guilds = client.guilds.cache.array().map((G, I) => `${I + 1}. **${G.name}** - **${G.id}**`).join("\n");
+  execute(message) {
+const Discord = require("discord.js");
+const client = new Discord.Client();
+        const Guilds = message.client.guilds.cache.array().map((G, I) => `${I + 1}. **${G.name}** - **${G.id}**`).join("\n");
     if (!Guilds) return message.channel.send("No Guild");
     return message.channel.send(Guilds, { split: { char: "\n" } }); 
   }
