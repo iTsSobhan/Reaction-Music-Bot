05a908eb13f1d3668b144d172a544557cfeaa136
Sobhan.SRZA <90289153+Sobhan-SRZA@users.noreply.github.com>
2022-01-07 18:45:42 +0330

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -7,29 +7,27 @@ module.exports = {
   name: "about",
   aliases: ["info","bot info"],
   description: i18n.__('show you about bot'),
-  execute(message,client) {
+  execute(message) {
 const Discord = require("discord.js");
 const moment = require("moment");
-let infoEmbed = new Discord.MessageEmbed()
+const client = new Discord.Client();
+    let infoEmbed = new Discord.MessageEmbed()
       infoEmbed.setColor("RANDOM");
-      infoEmbed.setTitle(`Stats from \`${client.user.username}\``);
-      infoEmbed.addField(":ping_pong: Ping",`┕\`${Math.round(client.ws.ping)}ms\``,true);
-
+      infoEmbed.setTitle(`About \`${message.client.user.username}\``);
+      infoEmbed.addField(":ping_pong: Ping",`┕\`${Math.round(message.client.ws.ping)}ms\``,true);
       infoEmbed.addField(":clock1: Uptime", `┕\`${moment.duration(message.client.uptime)}\``,true);
       infoEmbed.addField(":file_cabinet: Memory",`┕\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(
             2
           )}mb\``,true);
-
-      infoEmbed.addField(":homes: Servers",`┕\`${client.guilds.cache.size}\``, true);
-      infoEmbed.addField(":busts_in_silhouette: Users",`┕\`${client.users.cache.size}\``,true);
+      infoEmbed.addField(":homes: Servers",`┕\`${message.client.guilds.cache.size}\``, true);
+      infoEmbed.addField(":busts_in_silhouette: Users",`┕\`${message.client.users.cache.size}\``,true);
       infoEmbed.addField(":control_knobs: API Latency",`┕\`${message.client.ws.ping}ms\``,true);
-      infoEmbed.addField(":robot: Version",`┕\`Omega 5.2.1\``,true);
-
-      infoEmbed.addField(":blue_book: Discord.js",`┕\`v12.2.1\``,true);
-
-          infoEmbed.addField(":green_book: Node",`┕\`16.1.2\``,true);
+      infoEmbed.addField(":robot: Version",`┕\`Ultra 10.1.0\``,true);
+      infoEmbed.addField(":blue_book: Discord.js",`┕\`12.5.1\``,true);
+      infoEmbed.addField(":green_book: Node",`┕\`16.2.1\``,true);
+      infoEmbed.addField("👨🏻‍🎨 Create",`┕\`By ™️Sizar Team **Mr.SIN RE#1528**\``,true);
       infoEmbed.setTimestamp();
-      
+      infoEmbed.setFooter(`Requested by ${message.author.username}`, `${message.author.displayAvatarURL()}`);
             message.channel.send(infoEmbed)
   }
 }
