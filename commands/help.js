7090bfa8c89468dc9912f9737631dc1b69fef618
Sobhan.SRZA <90289153+Sobhan-SRZA@users.noreply.github.com>
2022-01-28 02:34:46 +0330

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require("discord.js");
 const { support_server } = require("../config.json");
-const { LOCALE } = require("../util/AdUtil");
+const { LOCALE, PREFIX } = require("../util/AdUtil");
 const i18n = require("i18n");
 
 i18n.setLocale(LOCALE);
@@ -9,9 +9,12 @@ module.exports = {
   name: "help",
   aliases: ["h"],
   description: i18n.__("help.description"),
-  execute(message) {
+async  execute(message) {
     let commands = message.client.commands.array();
 
+      const db = require("quick.db");
+    var prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`);
+    
     let helpEmbed = new MessageEmbed()
       .setAuthor(`Requested by ${message.author.username}`, `${message.author.displayAvatarURL()}`)
       .setTitle(i18n.__mf("help.embedTitle", { botname: message.client.user.username }))
@@ -20,10 +23,10 @@ module.exports = {
       .setColor("RANDOM")
       .addField(`**Links**`, `**[Support Server](${support_server || "https://discord.gg/5GYNec4urW"}) • [Invite](https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=412353895745&scope=bot)**`,false)
 
-      .setFooter(`To get info of each command you can do ${message.client.prefix}help | Create by Mr.SIN RE#1528 :)`);
+      .setFooter(`To get info of each command you can do ${prefix}help | Create by Mr.SIN RE#1528 :)`);
     commands.forEach((cmd) => {
       helpEmbed.addField(
-        `**${message.client.prefix}${cmd.name}**`,
+        `**${prefix}${cmd.name}**`,
         `\`Description: ${cmd.description} | Aliases: (${cmd.aliases ? `${cmd.aliases}` : ""})\``,
         true
       );
